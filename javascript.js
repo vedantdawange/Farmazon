94e3fc4859c227ec75eb6f05314eb012f6731508
vedantdawange <vedantdawange@gmail.com>
2021-04-04 19:56:04 +0530

    updates JS

--- javascript.js
+++ javascript.js
@@ -1,52 +1,23 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-    apiKey: "AIzaSyDihtrxLmXqqzsVTpMfyv99go-RVijabOU",
-    authDomain: "farmazon-fde8e.firebaseapp.com",
-    projectId: "farmazon-fde8e",
-    storageBucket: "farmazon-fde8e.appspot.com",
-    messagingSenderId: "1055915554009",
-    appId: "1:1055915554009:web:e79951b1c71b670f53fac8",
-    measurementId: "G-61S94CLYJG"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+const u_name = document.getElementById('name');
+const email = document.getElementById('email');
 
+const password = document.getElementById('pass');
+const mobno = document.getElementById('mobno');
+const age = document.getElementById('age');
+const gender = document.getElementById('gender');
+const category = document.getElementById('category')
 
-const auth = firebase.auth();
+const database = firebase.database();
 
+signup.addEventListener('click', (e) => {
+    e.preventDefault();
+    database.ref('/user/' + email.value).set({
+        user_name: u_name.value,
+        pass_word: password.value,
+        mob_no: mobno.value,
+        age: age.value,
+        gender: gender.value,
+        category: category.value,
 
-function signUp() {
-
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-
-    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-    promise.catch(e => alert(e.message));
-
-    alert("Signed Up");
-}
-
-
-
-function signIn() {
-
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-
-    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-    promise.catch(e => alert(e.message));
-
-
-
-
-}
-
-
-function signOut() {
-
-    auth.signOut();
-    alert("Signed Out");
-
-}
+    })
+})
