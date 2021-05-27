35b8693d5195eb477944bf17912bf25eb43aae88
Vedant Dawange <59818584+vedantdawange@users.noreply.github.com>
2021-05-27 20:13:18 +0530

    back button added

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
 function GoogleSignIn() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function() {
-        window.location = "AfterLogin.html"
-    }).catch(function(error) {
-        var errormessage = error.message;
-        alert(errormessage);
-
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function () {
+      window.location = "AfterLogin.html";
     })
+    .catch(function (error) {
+      var errormessage = error.message;
+      alert(errormessage);
+    });
 }
+
+let btnback = document.getElementById("bckbttn");
+
+btnback.addEventListener("click", () => {
+  window.history.back();
+});
